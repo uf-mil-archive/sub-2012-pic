7ffd60d1848f278c11227c2d6b71aa333903aa64
Paul Launier <plaunier@ufl.edu>
2011-06-13 12:12:53 -0400

    Main Function changed for Merge Board

--- MergeBoardAPP/source/main.c
+++ MergeBoardAPP/source/main.c
@@ -1,3 +1,6 @@
+////////////////////////////////////////////////////////////////////////////////
+// Merge Board Main
+////////////////////////////////////////////////////////////////////////////////
 #include "p33Fxxxx.h"
 #include "HardwareProfile.h"
 
@@ -18,8 +21,18 @@ _FWDT(FWDTEN_OFF)
 _FICD(ICS_PGD3 & JTAGEN_OFF)
 _FPOR(PWMPIN_ON & HPOL_ON & LPOL_ON & ALTI2C_OFF)
 
-// Function declarations
+/*************************/
+/*  FUNCTION PROTOTYPES  */
+/*************************/
 static void clockInit(void);
+
+/**********************/
+/*  Global Variables  */
+/**********************/
+extern UINT16 ADC16Val[5];
+extern UINT16 ADC32Val[5];
+//I2C_DRV i2cfan = I2C_FANDRV_DEFAULTS;
+
 
 // C30 Exception Handlers
 // If your code gets here, you either tried to read or write
@@ -37,18 +50,19 @@ void _ISR __attribute__((__no_auto_psv__)) _StackError(void)
     Nop();
 }
 
-/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- GenerateRandomDWORD() is a nasty little function that takes control of
- ADC1. DMA will fail if it is configured and running before that function
- is called. It's only used by the TCP/IP stack init functions, so just
- take over the ADC after they have been called via StackInit(). However,
- StackInit() uses RTOS calls, and thus, the scheduler must be running!
- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ***/
+////////////////////////////////////////////////////////////////////////////////
+//                                 MAIN
+////////////////////////////////////////////////////////////////////////////////
 int main(void)
 {
     /* These should be the first two calls in main - always!*/
     clockInit();
     ioMap();
+    hallSwInit() ;              // Initialize HALL SW interupts
+
+    BUZZER = BUZZER_OFF;        // initial state of buzzer is off
+    LED = LED_OFF;              // initial state of debug LED is off
+
 
     LED_TRIS = OUTPUT_PIN;
     LED = LED_OFF;
@@ -83,39 +97,63 @@ void ioMap(void)
     // Map the Analog pins
     AD1PCFGL = ANCONFIG;
 
+    // set LED Pin as an output
+    LED_TRIS = OUTPUT_PIN;
+
+    // set Buzzer Pin as an output
+    BUZZER_TRIS = OUTPUT_PIN;
+
+    // Initialize the ADC CS pins and set them to fasle
+    ADC16_CS_TRIS = OUTPUT_PIN;
+    ADC16_CS_IO = AL_FALSE;
+
+    ADC32_CS_TRIS = OUTPUT_PIN;
+    ADC32_CS_IO = AL_FALSE;
+
+    // Initialize the Highside Controler pins as ouputs and turn them off
+    RAIL16_TRIS = OUTPUT_PIN;   //16V Rail Control
+    RAIL16 = TURN_OFF;
+
+    RAIL32_TRIS = OUTPUT_PIN ;   //32V Rail Control
+    RAIL32 = TURN_OFF;
+
+    ENET_SW_TRIS = OUTPUT_PIN;   //Ethernet Switch Control
+    ENET_SW = TURN_OFF;
+
+    /********** Initialize the open drain pins ***************/
+    ODCA = ODCACONFIG;
+    ODCB = ODCBCONFIG;
+    ODCC = ODCCCONFIG;
+
     // Clear the IOLOCK bit
     __builtin_write_OSCCONL(OSCCON & 0xFFBF);
 
-    /********** ENC SPI ***************/
 
+    /********** ENC SPI ***************/
     // Inputs
     ENC_SDI_PINREG = ENC_SDI_PIN;
     // Outputs
     ENC_SCK_PIN = ENC_SPIxSCK_IO;
-#if defined ENC_SDO_PIN
-    ENC_SDO_PIN = ENC_SPIxSDO_IO;
-#endif
-
+    #if defined ENC_SDO_PIN
+        ENC_SDO_PIN = ENC_SPIxSDO_IO;
+    #endif
 
     /********** Multiple Devices SPI ***************/
-
     // Inputs
     EROM_SDI_PINREG = EROM_SDI_PIN;
     // Outputs
     EROM_SCK_PIN = EROM_SPIxSCK_IO;
-#if defined EROM_SDO_PIN
-    EROM_SDO_PIN = EROM_SPIxSDO_IO;
-#endif
+    #if defined EROM_SDO_PIN
+        EROM_SDO_PIN = EROM_SPIxSDO_IO;
+    #endif
 
     /********** Communication UART ***************/
-
     // Inputs
     COM_URX_PINREG = COM_URX_PIN;
     // Outputs
     COM_UTX_PIN = COM_UxTX_IO;
 
-    /********** Comparator 2 ***************/
-    COMP2_OUT_PIN = COMP2_OUT;
+    //TRISCbits.TRISC7 = 0;   // UART TX TRIS for cheesyUART
 
     // Lock the IOLOCK bit so that the IO is not accidentally changed.
     __builtin_write_OSCCONL(OSCCON | 0x0040);
