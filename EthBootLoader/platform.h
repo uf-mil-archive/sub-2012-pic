a471353ef3c926cd873e2329434dc25bdb6edf2f
Devin <email@host.com>
2011-07-04 18:58:56 -0400

    Put shell for MERGE into UDP bootloader

--- EthBootLoader/platform.h
+++ EthBootLoader/platform.h
@@ -17,6 +17,7 @@
 //		Select the target platform
 //////////////////////////////////////////////////////////////////////////
 #define BDCMC
+//#define MERGE
 
 ///////////////////////////////////////////////////////////////////////////
 //		Specify the debug code to be included
@@ -157,6 +158,132 @@
         //********************* ~EEPROM SPI ***********************************/
 
 #endif // BDCMC platform
+
+#if defined MERGE
+
+	#ifndef __dsPIC33FJ128MC804__
+		#error "Hardware Profile for selected processor does not exist"
+	#endif // Allowable processors
+
+	// Don't forget to call clockinit for this board! This configures
+	// the dspic33f to use a 10MHz clock and get 40MIPs
+	#define USE_CLOCK_INIT
+	
+	#define XTAL_FREQ 		10000000
+	#define PLLMODE			8
+	
+	#define AD1PCFGL_Def	0xFFF0	// Default Analog pin configuration
+	
+	#define CONSOLE_BAUDRATE	115200
+	
+	// Define UART I/O
+	#define CONSOLE_UART 		2
+
+	// Map RP's properly
+	#define USE_IO_MAPPING	
+	// UART RP
+	#define UART2_RX_RP			21
+	#define UART2_TX_RPR		RPOR3bits.RP7R	
+	// ENCSPI RP
+	#define SPI2_SDI_RP			24
+	#define SPI2_CLK_RPR		RPOR11bits.RP22R
+	#define SPI2_SDO_RPR		RPOR11bits.RP23R
+
+	///////////////////////////////////////////////////////////////////////////
+	//		Default I/O configuration
+	///////////////////////////////////////////////////////////////////////////
+	#define	PA_DefData		0b0000011100011000
+	#define	PA_DefTRIS		0b1111100011100111
+		//	10		LED
+		//	9		EROM_WP
+		//	8		DAC_CS
+		//	4		EROM_CS
+		//	3		DA_LD
+	
+
+	#define	PB_DefData		0b0000001100000000
+	#define	PB_DefTRIS		0b0000000001111111
+		//	15	PWM		Turn this off!
+		//	14	PWM		Turn this off!
+		//	13	PWM		Turn this off!
+		//	12	PWM		Turn this off!
+		//	11	PWM		Turn this off!
+		//	10	PWM		Turn this off!
+		//	9	ENC_CS	
+		//	8	ENC_RESET
+		//	7	CONSOLE TX
+		//	4	EROMSPI_SDI	
+
+	#define	PC_DefData		0b0000000000000000
+	#define	PC_DefTRIS		0b1111111100100111	
+		//	8		ENCSPI_SDI	
+		//  7		ENCSPI_SDO	
+		//  6 		ENCSPI_CLK	
+		//	5		CONSOLE RX
+		// 	4		EROMSPI_SDO
+		//	3		EROMSPI_CLK
+
+	///////////////////////////////////////////////////////////////////////////
+	// define ENC SPI bus low and high speed prescalars
+	///////////////////////////////////////////////////////////////////////////
+	#define ENC_SPI_PRESCALE_PRI 2  	// Primary Scaler = 1:2
+	#define ENC_SPI_PRESCALE_SEC 7		// Secondary Scaler = 1:1
+	#define ENC_SPI_PORT	2
+	#define	ENC_CS			LATBbits.LATB9	// ENC Chip Select Active low
+
+	///////////////////////////////////////////////////////////////////////////
+	//		Status LEDs
+	///////////////////////////////////////////////////////////////////////////
+	#define LED_Status			LATAbits.LATA10
+	#define LED_Status_TRIS		TRISAbits.TRISA10
+	#define LED0 				LED_Status
+	#define LED0_TRIS 			LED_Status_TRIS
+
+        // Generic SPI Helper Defines
+        #define SPIREG2(a,b)    SPI##a##b
+        #define SPIREG(a,b)     SPIREG2(a,b)
+
+        // SPI PPS Outputs
+        #define SPI1SCK_IO	8
+        #define SPI1SDO_IO	7
+        #define SPI2SCK_IO	11
+        #define SPI2SDO_IO	10
+
+        //********************* EEPROM SPI *****************************/
+        // The EEPROM and the DAC used to set a reference current
+        // share the same SPI bus.
+
+        // The EEPROM digital pins
+        #define EROM_CS_TRIS    TRISAbits.TRISA4    // The EEPROM Chip Select TRIS
+        #define EROM_CS_IO	LATAbits.LATA4      // The EEPROM Chip Select IO
+
+        #define EROM_WP_TRIS	TRISAbits.TRISA9    // The EEPROM Write Protect TRIS
+        #define EROM_WP_IO	LATAbits.LATA9      // The EEPROM Write Protect IO
+
+        #define EROM_SPINUM	1   // You must define this number and the proper
+                                    // SDI PPS register below
+
+        // Define PPS SPI Pins
+        #define EROM_SDO_PIN    RPOR10bits.RP20R    // EEPROM SPI Data Out pin RP20
+        #define EROM_SCK_PIN    RPOR9bits.RP19R     // EEPROM SPI Clock pin RP19
+        #define EROM_SDI_PIN	4                   // EEPROM SPI Data In pin RP4
+        #define EROM_SDI_PINREG RPINR20bits.SDI1R   // The register where SDI
+                                                    // pin is assigned
+
+        // Map SPI Registers
+        #define EROM_SPIxSTAT           SPIREG(EROM_SPINUM,STAT)
+        #define EROM_SPIxCON1		SPIREG(EROM_SPINUM,CON1)
+        #define EROM_SPIxCON2		SPIREG(EROM_SPINUM,CON2)
+        #define EROM_SPIxSTATbits	SPIREG(EROM_SPINUM,STATbits)
+        #define EROM_SPIxCON1bits  	SPIREG(EROM_SPINUM,CON1bits)
+        #define EROM_SPIxCON2bits  	SPIREG(EROM_SPINUM,CON2bits)
+        #define EROM_SPIxBUF     	SPIREG(EROM_SPINUM,BUF)
+        #define EROM_SPIxSCK_IO  	SPIREG(EROM_SPINUM,SCK_IO)
+        #define EROM_SPIxSDO_IO  	SPIREG(EROM_SPINUM,SDO_IO)
+
+        //********************* ~EEPROM SPI ***********************************/
+
+#endif // MERGE platform
 
 ///////////////////////////////////////////////////////////////////////////
 //		End of platform specific definititions
